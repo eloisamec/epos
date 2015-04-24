cf38d35de1655e9e8df452c72ba647f365419eb6
eloisamec <eloisa.mec@gmail.com>
2015-04-23 23:10:48 -0300

    novo exercicio

---
+++ epos exercicio 4/include/utility/handler.h
@@ -0,0 +1,36 @@
+// EPOS Handler Utility Declarations
+
+#ifndef __handler_h
+#define __handler_h
+
+#include <system/config.h>
+
+__BEGIN_UTIL
+
+class Handler {
+
+public:
+	typedef void (Function)();
+	
+	Handler() {}
+    virtual ~Handler() {}
+	
+	virtual void operator()() = 0;
+};
+
+class Function_Handler : public Handler {
+	
+public:
+	Function_Handler(Function * handler) : _handler(handler) {};
+	~Function_Handler() {};
+	
+	void operator()() {
+		_handler();
+	}
+private:
+	Function* _handler;
+};
+
+__END_UTIL
+
+#endif
